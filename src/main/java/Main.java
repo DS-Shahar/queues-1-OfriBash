70f3de7e1fb0b2e2a285de0bf885bfd08de64a45
OfriBash <126421560+OfriBash@users.noreply.github.com>
2024-02-25 09:51:58 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,23 @@
 class Main {
+  	public static boolean q1(int num, Queue <Integer> q) {
+		q.insert(null);
+		boolean seq = false;
+		
+		while(q.head() != null) {
+			if (q.head() == num) {
+				q.insert(q.remove());
+				if(q.head() == num)
+					seq = true;
+			}
+			if(q.head() != null)
+				q.insert(q.remove());	
+		}
+		q.remove();
+		return seq;
+	}
+
+
+  
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
